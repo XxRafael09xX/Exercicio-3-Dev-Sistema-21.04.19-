f52dab02416761011a84601786a919f6d3824b21
Rafael Silva <rafajogos14@yahoo.com.br>
2019-04-21 12:02:17 -0300

    1

--- Exercicio  3 (Dev Sistema 21.04.19)/Form1.Designer.vb
+++ Exercicio  3 (Dev Sistema 21.04.19)/Form1.Designer.vb
@@ -22,10 +22,185 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.components = New System.ComponentModel.Container()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.ToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AdiçãoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SubtraçãoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MultiplicaçãoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.DivisãoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.NovoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AjudaToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CréditosToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.MenuStrip1.SuspendLayout()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripMenuItem1, Me.NovoToolStripMenuItem, Me.AjudaToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(357, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'ToolStripMenuItem1
+        '
+        Me.ToolStripMenuItem1.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AdiçãoToolStripMenuItem, Me.SubtraçãoToolStripMenuItem, Me.MultiplicaçãoToolStripMenuItem, Me.DivisãoToolStripMenuItem})
+        Me.ToolStripMenuItem1.Name = "ToolStripMenuItem1"
+        Me.ToolStripMenuItem1.Size = New System.Drawing.Size(64, 20)
+        Me.ToolStripMenuItem1.Text = "Tabuada"
+        '
+        'AdiçãoToolStripMenuItem
+        '
+        Me.AdiçãoToolStripMenuItem.Name = "AdiçãoToolStripMenuItem"
+        Me.AdiçãoToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.A), System.Windows.Forms.Keys)
+        Me.AdiçãoToolStripMenuItem.Size = New System.Drawing.Size(191, 22)
+        Me.AdiçãoToolStripMenuItem.Text = "Adição"
+        '
+        'SubtraçãoToolStripMenuItem
+        '
+        Me.SubtraçãoToolStripMenuItem.Name = "SubtraçãoToolStripMenuItem"
+        Me.SubtraçãoToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.S), System.Windows.Forms.Keys)
+        Me.SubtraçãoToolStripMenuItem.Size = New System.Drawing.Size(191, 22)
+        Me.SubtraçãoToolStripMenuItem.Text = "Subtração"
+        '
+        'MultiplicaçãoToolStripMenuItem
+        '
+        Me.MultiplicaçãoToolStripMenuItem.Name = "MultiplicaçãoToolStripMenuItem"
+        Me.MultiplicaçãoToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.M), System.Windows.Forms.Keys)
+        Me.MultiplicaçãoToolStripMenuItem.Size = New System.Drawing.Size(191, 22)
+        Me.MultiplicaçãoToolStripMenuItem.Text = "Multiplicação"
+        '
+        'DivisãoToolStripMenuItem
+        '
+        Me.DivisãoToolStripMenuItem.Name = "DivisãoToolStripMenuItem"
+        Me.DivisãoToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.D), System.Windows.Forms.Keys)
+        Me.DivisãoToolStripMenuItem.Size = New System.Drawing.Size(191, 22)
+        Me.DivisãoToolStripMenuItem.Text = "Divisão"
+        '
+        'NovoToolStripMenuItem
+        '
+        Me.NovoToolStripMenuItem.Name = "NovoToolStripMenuItem"
+        Me.NovoToolStripMenuItem.Size = New System.Drawing.Size(48, 20)
+        Me.NovoToolStripMenuItem.Text = "Novo"
+        '
+        'AjudaToolStripMenuItem
+        '
+        Me.AjudaToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.CréditosToolStripMenuItem})
+        Me.AjudaToolStripMenuItem.Name = "AjudaToolStripMenuItem"
+        Me.AjudaToolStripMenuItem.Size = New System.Drawing.Size(50, 20)
+        Me.AjudaToolStripMenuItem.Text = "Ajuda"
+        '
+        'CréditosToolStripMenuItem
+        '
+        Me.CréditosToolStripMenuItem.Name = "CréditosToolStripMenuItem"
+        Me.CréditosToolStripMenuItem.Size = New System.Drawing.Size(118, 22)
+        Me.CréditosToolStripMenuItem.Text = "Créditos"
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(61, 4)
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(9, 41)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(44, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Número"
+        '
+        'NumericUpDown1
+        '
+        Me.NumericUpDown1.Location = New System.Drawing.Point(12, 57)
+        Me.NumericUpDown1.Name = "NumericUpDown1"
+        Me.NumericUpDown1.Size = New System.Drawing.Size(41, 20)
+        Me.NumericUpDown1.TabIndex = 2
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"+", "-", "*", "/"})
+        Me.ComboBox1.Location = New System.Drawing.Point(78, 57)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox1.TabIndex = 3
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(219, 57)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(123, 23)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "="
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(12, 99)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(330, 199)
+        Me.ListBox1.TabIndex = 5
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(75, 41)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(51, 13)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Operador"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(357, 310)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.NumericUpDown1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Tabuada Versão 1.0"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents ToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents ContextMenuStrip1 As ContextMenuStrip
+    Friend WithEvents AdiçãoToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents MultiplicaçãoToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents DivisãoToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents NovoToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AjudaToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CréditosToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Label1 As Label
+    Friend WithEvents NumericUpDown1 As NumericUpDown
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents SubtraçãoToolStripMenuItem As ToolStripMenuItem
 End Class
--- Exercicio  3 (Dev Sistema 21.04.19)/Form1.vb
+++ Exercicio  3 (Dev Sistema 21.04.19)/Form1.vb
@@ -1,3 +1,66 @@
 ﻿Public Class Form1
 
+
+    Private Sub CréditosToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CréditosToolStripMenuItem.Click
+        Form2.Show()
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim cont As Integer = NumericUpDown1.Value
+        If ComboBox1.Text() = "+" Then
+            For index = 1 To 10
+                ListBox1.Items.Add(NumericUpDown1.Value & ComboBox1.Text() & index & "=" & NumericUpDown1.Value + ++index)
+            Next
+
+        ElseIf ComboBox1.Text() = "-" Then
+            For index = 1 To 10
+                cont = cont + 1
+                ListBox1.Items.Add(cont & ComboBox1.Text() & NumericUpDown1.Value & "=" & (cont - NumericUpDown1.Value))
+            Next
+
+        ElseIf ComboBox1.Text() = "*" Then
+            For index = 1 To 10
+                ListBox1.Items.Add(NumericUpDown1.Value & ComboBox1.Text() & index & "=" & NumericUpDown1.Value * ++index)
+            Next
+
+        ElseIf ComboBox1.Text() = "/" Then
+
+            For index = 1 To 10
+                ListBox1.Items.Add(cont & ComboBox1.Text() & NumericUpDown1.Value & "=" & (cont / NumericUpDown1.Value))
+                cont = cont + 4
+            Next
+
+        End If
+    End Sub
+
+    Private Sub NovoToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles NovoToolStripMenuItem.Click
+        ListBox1.Items.Clear()
+        NumericUpDown1.Value = NumericUpDown1.Minimum
+        ComboBox1.ResetText()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        NumericUpDown1.Maximum = 9
+        NumericUpDown1.Minimum = 1
+    End Sub
+
+
+    Private Sub AdiçãoToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AdiçãoToolStripMenuItem.Click
+        ComboBox1.Text = "+"
+    End Sub
+
+    Private Sub SubtraçãoToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SubtraçãoToolStripMenuItem.Click
+        ComboBox1.Text = "-"
+    End Sub
+
+    Private Sub MultiplicaçãoToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles MultiplicaçãoToolStripMenuItem.Click
+        ComboBox1.Text = "*"
+    End Sub
+
+    Private Sub DivisãoToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles DivisãoToolStripMenuItem.Click
+        ComboBox1.Text = "/"
+    End Sub
+
+
 End Class
